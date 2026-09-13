5bacd9e924bbc3c7a512811b5049a2b1e69bf53a
AndreyDeveloper84 <42996467+AndreyDeveloper84@users.noreply.github.com>
2026-09-13 12:44:49 +0300

    DRF-1862 export generation service

--- apps/core/services/__init__.py
+++ apps/core/services/__init__.py
@@ -1,7 +1,10 @@
+from .generation import GenerationError, GenerationService
 from .order_state import InvalidOrderTransition, OrderStateService
 from .payment import PaymentError, PaymentService
 
 __all__ = [
+    "GenerationError",
+    "GenerationService",
     "InvalidOrderTransition",
     "OrderStateService",
     "PaymentError",
