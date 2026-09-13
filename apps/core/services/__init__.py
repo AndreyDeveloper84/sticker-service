7611ab8cf0fc23c9b66196b5d3c6828b5ba20093
AndreyDeveloper84 <42996467+AndreyDeveloper84@users.noreply.github.com>
2026-09-13 12:06:32 +0300

    DRF-1859 export payment service

--- apps/core/services/__init__.py
+++ apps/core/services/__init__.py
@@ -1,3 +1,9 @@
 from .order_state import InvalidOrderTransition, OrderStateService
+from .payment import PaymentError, PaymentService
 
-__all__ = ["InvalidOrderTransition", "OrderStateService"]
+__all__ = [
+    "InvalidOrderTransition",
+    "OrderStateService",
+    "PaymentError",
+    "PaymentService",
+]
