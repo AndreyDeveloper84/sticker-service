9ec6d4bf21d8035eb8163236f9285e7d2abc2139
AndreyDeveloper84 <42996467+AndreyDeveloper84@users.noreply.github.com>
2026-09-13 11:48:10 +0300

    DRF-1825 install production console

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.core"
+
+    def ready(self):
+        from .production_console import install_production_console
+
+        install_production_console()
